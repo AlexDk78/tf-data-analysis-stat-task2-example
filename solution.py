c651b6a7501afd5120402c6e77f184887e65a8b7
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-01 22:06:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,15 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
-
-
 chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    from scipy.stats import t
     n = len(x)
-    mean_x = np.mean(x)
-    std_x = np.std(x, ddof=1)
-    t = norm.ppf(1 - (1-p)/2)
-    interval = t * std_x / np.sqrt(n)
-    left_limit = mean_x - interval
-    right_limit = mean_x + interval
-    return (left_limit, right_limit)
+    mean = np.mean(x)
+    std = np.std(x, ddof=1)
+    t_value = t.ppf(1 - (1-p)/2, n-1)
+    margin_of_error = t_value * std / np.sqrt(n)
+    lower_limit = mean - margin_of_error
+    upper_limit = mean + margin_of_error
+    return (lower_limit, upper_limit)
