1a9ee585570e209d14ed3e912e4682f9d74a5feb
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-04 21:16:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,12 +4,9 @@ import numpy as np
 chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    from scipy.stats import t
+    alpha = 1 - p
     n = len(x)
-    mean = np.mean(x)
-    std = np.std(x, ddof=1)
-    t_value = t.ppf(1 - (1-p)/2, n-1)
-    margin_of_error = t_value * std / np.sqrt(n)
-    lower_limit = mean - margin_of_error
-    upper_limit = mean + margin_of_error
-    return (lower_limit, upper_limit)
+    left = (-min(-x) - 1 / 2) / (92**2 / 2)
+    right = (-np.log(alpha) / n -min(-x) - 1 / 2) / (92**2 / 2)
+    return left, \
+            right
